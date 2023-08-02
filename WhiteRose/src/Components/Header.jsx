47174a7b8f22ad664530a959a1a359c0643a4181
Thando1992 <nothandomkhize52@gmail.com>
2023-08-02 11:18:13 +0200

    navbar-collapse added

--- WhiteRose/src/Components/Header.jsx
+++ WhiteRose/src/Components/Header.jsx
@@ -1,18 +1,18 @@
- import React from 'react'
- import '../Components/Header.css'
+import React from 'react'
+import '../Components/Header.css'
 
 function Header() {
-     return (
+    return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
 
-          <div className="container-fluid">
+            <div className="container-fluid">
                 <a className="navbar-brand" href="#">
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjhJFgQR2bhOXVdWg-VqT0AphwEa6Mkq_AqPCofLb47a77-VzjUn8iLeGMCuuCIEsv1Gs&usqp=CAU" alt="My logo" id="nav" />
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                     <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-//                 <div className="collapse navbar-collapse" id="navbarNav">
+                <div className="collapse navbar-collapse" id="navbarNav">
 //                     <ul className="navbar-nav">
 //                         <li className="nav-item">
 //                             <a className="nav-link active" aria-current="page" href="/">Home</a>
@@ -55,11 +55,11 @@ function Header() {
 //                 </div>
 //             </div>
 //             <div>
-//             </div> */}
+//             </div>
 
-       </nav>
+        </nav>
 
- )
+    )
 }
 
 export default Header
