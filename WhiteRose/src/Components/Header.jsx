7e6ab01bb0a146b482f9543cb96b5e62ff9a5c91
Thando1992 <nothandomkhize52@gmail.com>
2023-08-02 11:23:24 +0200

    home added

--- WhiteRose/src/Components/Header.jsx
+++ WhiteRose/src/Components/Header.jsx
@@ -14,9 +14,9 @@ function Header() {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
-//                         <li className="nav-item">
-//                             <a className="nav-link active" aria-current="page" href="/">Home</a>
-//                         </li>
+                        <li className="nav-item">
+                             <a className="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
 //                         <li className="nav-item">
 //                             <a className="nav-link" href="/pricelist">Price List</a>
 //                         </li>
